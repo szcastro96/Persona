1250c1b75606360ffb05a68dc4f31961ada12d99
szcastro96 <szcastro96@gmail.com>
2018-05-20 23:45:06 -0600

    labo06, que no terminamos pero ahorita si

--- app/src/main/java/com/gamma/persona/ModificarActivity.java
+++ app/src/main/java/com/gamma/persona/ModificarActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.gamma.persona.Datos.Persona;
 import com.gamma.persona.entidades.DBHelper;
@@ -26,11 +27,36 @@ public class ModificarActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Persona p = DBHelper.myDB.findUser(id.getText().toString());
                 if (p == null){
-                    
+                    Toast.makeText(getApplicationContext(), "El usuario no fue encontrado", Toast.LENGTH_LONG).show();
+                    limpiar();
                 }
+                else{
+                    nombre.setText(p.getNombre());
+            }
             }
         });
 
+        btnActualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DBHelper.myDB.editUser(new Persona(id.getText().toString(),nombre.getText().toString()));
+            }
+        });
+
+        btnEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DBHelper.myDB.deleteUser(id.getText().toString());
+                limpiar();
+            }
+        });
+
+        btnLimpiar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                limpiar();
+            }
+        });
     }
 
     public void inicializarControles(){
